9a6bd117b02e2f6481bdcb7d98d4a99b5eec1fee
Daoan XU <daoan.xu@gmail.com>
2013-03-04 00:30:33 -0500

    Remove Nth Node From End of List: Recursive Solution, Stack Size O(N)

--- src/remove_nth_node_from_end_of_list/SolutionStack.java
+++ src/remove_nth_node_from_end_of_list/SolutionStack.java
@@ -1,5 +1,40 @@
 package remove_nth_node_from_end_of_list;
-
 public class SolutionStack {
+    class ListNode {
+        int val;
+        ListNode next;
 
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    private ListNode result;
+    private ListNode head;
+    private int count;
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        count = 0;
+        result = head;
+        this.head = head;
+        findit(head,n);
+        
+        return result;
+    }
+    
+    private void findit(ListNode current, int n){
+        if(current == null){
+            count = 0;
+            return;
+        }
+        findit(current.next,n);
+        count++;
+        if(count == n){
+            if(current == head)
+                result = head.next;
+        }
+        if(count == n+1){
+            current.next = current.next.next;
+        }
+    }
 }
