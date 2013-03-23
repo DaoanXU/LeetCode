c386ececd580c8966e7a42657adfdbb2c9d7554e
Daoan XU <daoan.xu@gmail.com>
2013-03-23 02:46:09 -0400

    Next PermutationFeb: Pass

---
+++ src/next_permutation/Solution.java
@@ -0,0 +1,26 @@
+package next_permutation;
+
+import java.util.Arrays;
+
+public class Solution {
+    public void nextPermutation(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int last = num.length - 1;
+        
+        for(int i = last; i > 0 ; i --){
+            Arrays.sort(num,i,last+1);
+            if(num[last] > num[i-1]){
+                for(int j = i ; j < last + 1 ; j ++){
+                    if(num[j] > num[i-1]){
+                        num[j] = num[i-1] ^ num[j];
+                        num[i-1] = num[i-1] ^ num[j];
+                        num[j] = num[i-1] ^ num[j];
+                        return;
+                    }
+                }                
+            }
+        }
+        Arrays.sort(num,0,last+1);
+    }
+}
