ffd0e09c44929adfbeefe1d8e661a5a4e45bcb06
Daoan XU <daoan.xu@gmail.com>
2013-05-28 13:02:22 -0700

    Container With Most Water : Pass

--- src/container_with_most_water/Solution.java
+++ src/container_with_most_water/Solution.java
@@ -1,9 +1,8 @@
 package container_with_most_water;
 
 public class Solution {
+    
     public int maxArea(int[] height) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
         return maxArea(height, 0, height.length - 1, 0);
     }
 
@@ -18,17 +17,17 @@ public class Solution {
             return maxArea(height, start, end - 1, max);
         return maxArea(height, start + 1, end, max);
     }
-    
-    public int maxArea2(int[] height, int start, int end, int max) {        
-        while (end > start){
+
+    public int maxArea2(int[] height, int start, int end, int max) {
+        while (end > start) {
             int thisArea = Math.min(height[start], height[end]) * (end - start);
             if (thisArea > max)
                 max = thisArea;
             if (height[start] > height[end])
                 end--;
             else
-                start++;            
-        }        
+                start++;
+        }
         return max;
     }
 }
