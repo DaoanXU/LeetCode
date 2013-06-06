fa8aa46cf591f45b1c3653bc28a3cdcf6d396b01
Daoan XU <daoan.xu@gmail.com>
2013-06-05 23:00:19 -0700

    Binary Tree Level Order Traversal : Pass

---
+++ src/binary_tree_level_order_traversal/Solution.java
@@ -0,0 +1,66 @@
+package binary_tree_level_order_traversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import _dependency.TreeNode;
+
+public class Solution {
+
+    class TreeIterator {
+
+        Queue<TreeNode> queue;
+
+        TreeIterator(TreeNode node) {
+            queue = new LinkedList<TreeNode>();
+            queue.offer(node);
+            if (node != null)
+                queue.offer(null);
+        }
+
+        boolean hasNext() {
+            return !queue.isEmpty();
+        }
+
+        TreeNode next() {
+            TreeNode head = queue.poll();
+            if (head == null) {
+                if (!queue.isEmpty())
+                    queue.offer(null);
+                return null;
+            }
+
+            if (head.left != null)
+                queue.offer(head.left);
+            if (head.right != null)
+                queue.offer(head.right);
+            return head;
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> row = new ArrayList<Integer>();
+
+        if (root == null) {
+            return result;
+        }
+
+        TreeIterator iterator = new TreeIterator(root);
+
+        while (iterator.hasNext()) {
+            TreeNode current = iterator.next();
+            if (current == null) {
+                result.add(row);
+                row = new ArrayList<Integer>();
+            }
+            else {
+                row.add(current.val);
+            }
+        }
+
+        return result;
+    }
+}
